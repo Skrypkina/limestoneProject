7e21c088e0386ffbfce4ba4ecf9eb9daf693ad89
Larysa <Larysa76@gmail.com>
2020-01-15 16:23:34 +0200

    completed project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import './styles.css';
+import './css/styles.css';
+require('bootstrap');
+
+('use strict');
+
+const Handlebars = require('handlebars');
+const grid = document.querySelector('.row');
+const source = document.querySelector('#user-card').innerHTML.trim();
+const template = Handlebars.compile(source);
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => {
+    if (res.ok) return res.json();
+    throw new Error('Error while fetching ' + response.statusText);
+  })
+  .then(data => {
+    // console.log(data);
+    const markup = data.reduce((acc, arr) => acc + template(arr), '');
+    grid.insertAdjacentHTML('afterbegin', markup);
+  })
+  .catch(error => {
+    console.log(error);
+  });
